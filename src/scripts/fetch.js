b4093a0c984a5fa7896fe709cc0a838155300bad
sergaik-web <seroga8863@gmail.com>
2020-04-16 17:37:52 +0300

    адд script

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,13 +1,30 @@
-export default class CreateReqest {
+class GodService {
     constructor (){
         this.statickUrl = 'https://www.anapioficeandfire.com/api'
     }
 
-    async onFetch(){
+    async onFetch(url){
         const res = await fetch(`${this.statickUrl}/characters?page=6&pageSize=10`);
+        if (!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
         return await res.json();
+    }
+
+    getAllCharacters(){
+        return this.onFetch(`/characters?page=7&pageSize=10`)
+    }
+
+    getCharacter(id){
+        return this.onFetch(`/characters/${id}`)
     }
 }
 
-const code = new CreateReqest();
-console.log(code.onFetch());
+const got = new GodService();
+got.getAllCharacters()
+    .then(res=>{
+        res.forEach(e=>console.log(e.name))
+    });
+
+got.getCharacter(prompt('Введите ID персонажа'))
+    .then(res=>console.log(res));
